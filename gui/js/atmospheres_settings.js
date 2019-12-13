27d7b25cc0394419e2d409fb48201daf23806e28
Hannes Kruse <Hannes.Kruse@dlr.de>
2019-12-13 16:01:46 +0100

    :construction: Gui refactoring

--- gui/js/atmospheres_settings.js
+++ gui/js/atmospheres_settings.js
@@ -1,2 +1,2 @@
-init_slider("set_atmosphere_quality", 1, 30, 1, [7]);
-init_slider("set_water_level", -2, 2, 0.01, [0]);
+CosmoScout.initSlider("set_atmosphere_quality", 1, 30, 1, [7]);
+CosmoScout.initSlider("set_water_level", -2, 2, 0.01, [0]);
